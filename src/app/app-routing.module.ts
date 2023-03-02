a2a9d4ae6d91799db4f5afb3acf6ea9d804b3778
sorin.nemtanu <sorin.nemtanu@bearingpoint.com>
2023-03-02 14:02:26 +0200

    components added ( home-page, posts, post and header ) and routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostComponent } from './post/post.component';
+import { PostsComponent } from './posts/posts.component';
+
+export const routes: Routes = [
+  { path: "home", component: HomePageComponent },
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "posts", component: PostsComponent },
+  { path: "posts/:id", component: PostComponent },
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './post/post.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    PostsComponent,
+    PostComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
